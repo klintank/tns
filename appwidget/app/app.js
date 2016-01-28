7bebd050d2f4127be45b619df2aeab06306a1a78
katturaja <katturaja@Katturaja>
2016-01-28 19:27:24 +0530

    xZXjZVZxjkvZvxVx

--- appwidget/app/app.js
+++ appwidget/app/app.js
@@ -2,7 +2,7 @@ var application = require('application');
 
 application.mainModule = 'navigation/navigation';
 
-//Hleooodasads
+
 
 // START_CUSTOM_CODE_nativeScriptApp
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
